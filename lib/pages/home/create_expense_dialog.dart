bcc95547b1bafa6fce50d84025d4ce22042925ac
leomartins1999 <asmartins.leonardo1999@gmail.com>
2022-11-11 00:55:28 +0000

    Minor refactors

--- lib/pages/home/create_expense_dialog.dart
+++ lib/pages/home/create_expense_dialog.dart
@@ -24,62 +24,71 @@ class _CreateExpenseDialogState extends State<CreateExpenseDialog> {
       title: const Text("Add new expense"),
       content: Column(
         mainAxisSize: MainAxisSize.min,
-        children: [
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: "Title",
-            ),
-            keyboardType: TextInputType.name,
-            onChanged: (v) => setState(() {
-              title = v;
-            }),
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: "Cost",
-            ),
-            keyboardType: TextInputType.number,
-            onChanged: (v) => setState(() {
-              value = double.parse(v);
-            }),
-          ),
-          TextFormField(
-            controller: dateTextController,
-            decoration: const InputDecoration(
-              labelText: "Date",
-              icon: Icon(Icons.calendar_today),
-            ),
-            readOnly: true,
-            onTap: () async {
-              final newDate = await showDatePicker(
-                context: context,
-                initialDate: date,
-                firstDate: DateTime(2000),
-                lastDate: DateTime(2101),
-              );
-
-              if (newDate == null) return;
-
-              setState(() {
-                dateTextController.text = newDate.format();
-                date = newDate;
-              });
-            },
-          )
-        ],
+        children: [buildTitleForm(), buildCostForm(), buildDateForm()],
       ),
-      actions: [
-        TextButton(
-          onPressed: !isAddEnabled()
-              ? null
-              : () {
-                  final expense = Expense(title!, value!, date);
+      actions: [buildAddButton()],
+    );
+  }
 
-                  return Navigator.of(context).pop(expense);
-                },
-          child: const Text("Add"),
-        ),
-      ],
+  Widget buildTitleForm() {
+    return TextFormField(
+      decoration: const InputDecoration(
+        labelText: "Title",
+      ),
+      keyboardType: TextInputType.name,
+      onChanged: (v) => setState(() {
+        title = v;
+      }),
+    );
+  }
+
+  Widget buildCostForm() {
+    return TextFormField(
+      decoration: const InputDecoration(
+        labelText: "Cost",
+      ),
+      keyboardType: TextInputType.number,
+      onChanged: (v) => setState(() {
+        value = double.parse(v);
+      }),
+    );
+  }
+
+  Widget buildDateForm() {
+    return TextFormField(
+      controller: dateTextController,
+      decoration: const InputDecoration(
+        labelText: "Date",
+        icon: Icon(Icons.calendar_today),
+      ),
+      readOnly: true,
+      onTap: () async {
+        final newDate = await showDatePicker(
+          context: context,
+          initialDate: date,
+          firstDate: DateTime(2000),
+          lastDate: DateTime(2101),
+        );
+
+        if (newDate == null) return;
+
+        setState(() {
+          dateTextController.text = newDate.format();
+          date = newDate;
+        });
+      },
+    );
+  }
+
+  Widget buildAddButton() {
+    onClick() {
+      final expense = Expense(title!, value!, date);
+      return Navigator.of(context).pop(expense);
+    }
+
+    return TextButton(
+      onPressed: isAddEnabled() ? onClick : null,
+      child: const Text("Add"),
     );
   }
 
--- lib/pages/home/expense_list.dart
+++ lib/pages/home/expense_list.dart
@@ -8,15 +8,15 @@ class ExpenseList extends StatelessWidget {
   const ExpenseList(this._expenses, {super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: _expenses
-          .map((e) => ListTile(
-                title: Text(e.title),
-                subtitle: Text(e.date.format()),
-                trailing: Text("${e.value.toStringAsFixed(2)} €"),
-              ))
-          .toList(),
-    );
+  Widget build(BuildContext context) => ListView(children: getListTiles());
+
+  List<ListTile> getListTiles() {
+    return _expenses
+        .map((e) => ListTile(
+              title: Text(e.title),
+              subtitle: Text(e.date.format()),
+              trailing: Text("${e.value.toStringAsFixed(2)} €"),
+            ))
+        .toList();
   }
 }
